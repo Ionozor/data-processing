bb47d3841b0d21db3877f9acbc59fc55f35954cd
IonozorVojta <laitl.fko@gmail.com>
2016-04-27 22:54:43 +0200

    Updates.

--- Octave/bin/one_shot-old.sh
+++ Octave/bin/one_shot-old.sh
@@ -2,7 +2,7 @@
 # Michal Žejdl 2016
 # run process_data-old.sh for days passed
 
-for location in HVEZDARNA OSTROV SUN; do
+for location in HVEZDARNA OSTROV SOKOLOV SUN; do
 	for yy in 15 16; do for mm in {01..12}; do for dd in {01..31}; do
 		~/Dropbox/Octave/bin/process_data-old.sh \
 			$location $yy $mm $dd
