9c57c77c1419ab6526bb0c50b9ec94ba9bcd6ec6
IonozorVojta <laitl.fko@gmail.com>
2016-05-26 23:21:58 +0200

    New SID.m file added.

--- Octave/bin/SID.m
+++ Octave/bin/SID.m
@@ -6,7 +6,6 @@ localpath = [local D localsid]
 cd(localpath)
 load 't.txt'
 load 'L.txt'
-t = 10.*t./10
 dt = [0 [diff(t)]]
 L1 = 0.1.*L
 n = -1/4.*L1.^-4
@@ -27,65 +26,65 @@ nu_Delta = p.*c./h
 nu = 23400 + nu_Delta 
 %DHO 38 band
 %Possible for every band which is determined before. E.g. nu = nu_Delta + ["DHO38" "NAA" "GBZ" ...].
-save -ascii critical_frequency_DHO38.txt nu
+save critical_frequency_DHO38.dat nu
 n_0 = (nu.^2*m_el*epsilon)/e^2
 n = -1/4.*L1.^-4 + n_0
-save -ascii electron_density_DHO38.txt n
-dn = [0; [diff(n)]
-T = -E_k1./k_B
+save electron_density_DHO38.dat n
+dn = [0 [diff(n)]]
+T = -E_k./k_B
 lambda_De = sqrt((epsilon.*k_B.*T)./(n.*e^2))
-save -ascii debye_radius_DHO38.txt lambda_De
+save debye_radius_DHO38.dat lambda_De
 T_el = T.*(n./lambda_De)
-save -ascii termodynamic_temperature_DHO38.txt T_el
+save termodynamic_temperature_DHO38.dat T_el
 N_D = 4/3*pi.*lambda_De.*n
-save -ascii plasma_parameter_DHO38.txt N_D
+save plasma_parameter_DHO38.dat N_D
 W_k = (3/2*k_B.*T_el)./e
-save -ascii kinetic_energy_DHO38.txt W_k
-p_el = [0;[diff(W_k)]]
-save -ascii linear_momentum_DHO38.txt p_el
+save kinetic_energy_DHO38.dat W_k
+p_el = [0 [diff(W_k)]]
+save linear_momentum_DHO38.dat p_el
 W_p = (2.*W_k)./(9.*N_D.^2/3)
-save -ascii potential_energy_DHO38.txt W_p1
+save potential_energy_DHO38.dat W_p
 v = (3*k_B.*T_el./(n.*m_el)).^1/2 .* lambda_De
-save -ascii mean_q_velocity_DHO38.txt v
-a = [0;[diff(v)]]
-save -ascii acceleration_DHO38.txt a
+save mean_q_velocity_DHO38.dat v
+ac = [0 [diff(v)]]
+save acceleration_DHO38.dat ac
 s = (2*k_B.*T_el.^3./(n.*m_el)).^1/2 .*lambda_De.^3
-save -ascii fluctuation_scope_DHO38.txt s
-A = ((dn.+n)./n)./(1000*N_A)
-B = R.*T
-E_a = abs(-log(A.^B))./e + h*1.21567*10^-7/e
-save -ascii activation_energy_DHO38.txt E_a
+save fluctuation_scope_DHO38.dat s
+A0 = ((dn.+n)./n)./(1000*N_A)
+B0 = R.*T
+E_a = abs(-log(A0.^B0))./e + h*1.21567*10^-7/e
+save activation_energy_DHO38.dat E_a
 U_i = E_a.*e
 i_1 = 2.4*10^21.*T_el.^3/2.*exp((-U_i.*lambda_De)./(n.*N_D.^3.*k_B.*T_el))
 i_2 = (2.4*10^21.*T_el.^3/2.*exp((-U_i.*lambda_De)./(n.*N_D.^3.*k_B.*T_el)).^3/4).^0.8584
 i_avrg = (i_1+i_2)./2
 i_all = [i_1 i_1 i_avrg]
-save -ascii ionization_degree_DHO38.txt i_all
-n_0 = omega^2*epsilon*m_el/e^2
-N = n-n0
-B = 1+(0.01.*L)
+save ionization_degree_DHO38.dat i_all
+n_0 = 23400^2*epsilon*m_el/e^2
+N = n-n_0
+Bl = 1+(0.01.*L)
 mD = 10^-10
-h0 = abs(pi*i.*B./(2+2.*mD))
+h0 = abs(pi*i.*Bl./(2+2.*mD))
 h0_t = (10.*h0).^2./10
-f_n = dn.*B./(N.*(1-mD))
+f_n = dn.*Bl./(N.*(1-mD))
 f_n2 = f_n.^2
 f_ln = log(1/2.*f_n-((f_n2.^2 - 4).^1/2))
-h_t = abs((B.*f_ln + 2.*pi.*i)./(1-mD))
+h_t = abs((Bl.*f_ln + 2.*pi.*i)./(1-mD))
 H = 10.*(h0_t + h_t)
-save -ascii height_DHO38.txt H
+save height_DHO38.dat H
 B1 = 10+(0.1.*L)
 f_H = H.*(1-mD)./B1
 exp1 = exp(f_H./10^11)
 exp2 = exp(-f_H./10^11)
 f_B = (1-mD)./B1
 n_H = L.^-5 + 4.*N.*f_B.*(exp1 + exp2)
-save -ascii height_dependent_TEC_DHO38.txt n_H
+save height_dependent_TEC_DHO38.dat n_H
 p = h0.*(mD-1)
 p1 = H.*(1-mD) + pi
-n_el = 10.*(exp((2.*N_D./1000.*(p1-p) + B)./B) + 4.*H)
+n_el = 10.*(exp((2.*N_D./1000.*(p1-p) + Bl)./Bl) + 4.*H)
 n_el1 = n_el./10^6
 T_e = 3^2/3.*e^2.*n_el.^1/3.*N_D.^2/3./(epsilon*k_B*(4*pi)^2/3).*lambda_De.^1.2
-n_e = 10.*(exp((2.*N_D./1000000.*(p1-p) + B)./B) + 4.*H)
+n_e = 10.*(exp((2.*N_D./1000000.*(p1-p) + Bl)./Bl) + 4.*H)
 n_e1 = n_e./10^6
 T_EL = 3^2/3.*e^2.*n_e.^1/3.*N_D.^2/3./(epsilon*k_B*(4*pi)^2/3).*lambda_De.^1.2
 lambda_M = (h*c.*lambda_De.^(N_D./100))./(k_B.*T_e)
@@ -94,68 +93,61 @@ f_nh = (-2.*H.*(mD-1) + h0_t.*(-mD) + h0_t + pi)./B
 f_exp = exp(f_nh)
 f_T = (-2^4/3.*(mD-1).*N.*(1-mD).*log10(2.71818281).*f_exp)./(3.*B.^2.*N.*(1-mD).*f_exp)
 dT = 3^2/3.*e^2.*N_D.^2/3./(epsilon.*k_B.*(4*pi)^2/3).*lambda_De.^6/5.*f_T
-dTe = [0;[diff(T_e)]]
+dTe = [0 [diff(T_e)]]
 Z = -1./dTe.*dT
 f1 = n_el.*E_a./Z
 f2 = k_B.*T_e
 f_I = f1.*f2.*exp(-T_el./T_e)
 I = abs(log(f_I))
-save -ascii intensity.txt I
-E = activation_energy + h.*nu_Delta./e
+save intensity.dat I
+E = E_a + h.*nu_Delta./e
 s_O2 = (2*9.339e012-9.312e007)/10^12
 s_N2 = (2*3.4711e+012-9.312e+007)/10^12
 s_N2O = (3*4.0732e012 - 9.312e+007)/10^12
 s_NO = (2*6.1824e+012 - 9.312e+007)/10^12
 s_CO2 = (2*6.9482e012 - 9.312e007)/10^12
-sl_HNO2 = lambda.-s_HNO2
 lambda = (h*c)./(E_a.*e.*lambda_De.^3./n.*N_D).*10^4
 sl_O2 = lambda.-s_O2
 sl_N2 = lambda.-s_N2
 sl_N2O = lambda.-s_N2O
 sl_NO = lambda.-s_NO
 sl_CO2 = lambda.-s_CO2
-sl_HNO2 = lambda.-s_HNO2
 E_O2 = h*c./(sl_O2.*10^-17)./e.*n.^(3.5.*N_D./100)
 E_N2 = h*c./(sl_N2.*10^-17)./e.*n.^(3.5.*N_D./100)
 E_N2O = h*c./(sl_N2O.*10^-17)./e.*n.^(3.5.*N_D./100)
 E_NO = h*c./(sl_NO.*10^-17)./e.*n.^(3.5.*N_D./100)
 E_CO2 = h*c./(sl_CO2.*10^-17)./e.*n.^(3.5.*N_D./100)
 E_all = [t E_O2 t E_N2 t E_N2O t E_NO t E_CO2 t E]
-save -ascii plasma_spectrum_DHO38.txt E_all
-tau = (dt.+t)./t
-F = E_a.*e./(tau.*n)
-save -ascii x_ray_flux_DHO38.txt F
-F_V = F./(lambda_De.*e)
-save -ascii x_ray_flux_in_debye_sphere_DHO38.txt F_V
+save plasma_spectrum_DHO38.dat E_all
 C_e = gamma.*n.*T_el
-save -ascii specific_calorific_capacity_DHO38.txt C_e
+save specific_calorific_capacity_DHO38.dat C_e
 Q = C_e.*m_el.*T./e
-save -ascii heat_DHO38.txt Q
-dQ = [0;[diff(Q)]]
+save heat_DHO38.dat Q
+dQ = [0 [diff(Q)]]
 S = dQ./T_el
-save -ascii termodynamic_entropy.txt S
+save termodynamic_entropy.dat S
 T = 10.*abs(log(b./lambda_M))
-save -ascii effective_temperature_meteors_DHO38.txt T
+save effective_temperature_meteors_DHO38.dat T
 lambda = b./T
-save -ascii wavelenghts_meteor_DHO38.txt lambda
-Z = log(((1.5*R_inf.*sqrt(3*R_inf*c./Lambda))./(1.5*R_inf)))-9
-save -ascii atoms_meteors_DHO38.txt Z
+save wavelenghts_meteor_DHO38.dat lambda
+Z = log(((1.5*R_inf.*sqrt(3*R_inf*c./lambda))./(1.5*R_inf)))-9
+save atoms_meteors_DHO38.dat Z
 dT = abs(10.*(log(T)))
 n = (16.*epsilon^3.*k_B^3.*pi^2.*lambda_De.^1.5.*exp(dT./(0.1.*N_D)))./(9.*e^6.*N_D.^2)
-save -ascii electron_density_meteors_DHO38.txt n
+save electron_density_meteors_DHO38.dat n
 dTS = 10.*abs(log(b./lambda_S))
 TS = exp(dTS./10)
-save -ascii temperature_solar_DHO38.txt TS
+save temperature_solar_DHO38.dat TS
 nS = (16.*epsilon^3.*k_B^3.*pi^2.*lambda_De.^1.5.*TS)./(9.*e^6.*N_D.^2)
-save -ascii electron_density_solar_DHO38.txt nS
+save electron_density_solar_DHO38.dat nS
 lambdas = b./TS
-save -ascii wavelenghts_sun_DHO38.txt lambda_S
+save wavelenghts_sun_DHO38.dat lambda_S
 ZS = abs(log(((1.5*R_inf.*sqrt(3*R_inf*c./lambdas))./(1.5*R_inf))) - 29)
-save -ascii atoms_solar_DHO38.txt ZS
+save atoms_solar_DHO38.dat ZS
 I0 = -26.74
 R = 1.496*10^11
 Is = exp(L./10)
 fce = Is.*R.^2./I0
 alpha = acos(fce)
 x = abs(H.*tan(alpha))
-save -ascii horizontal_drift_DHO38.txt x
+save horizontal_drift_DHO38.dat x
