4bfad010dff9fd8de336d7237b534e92ac5e965a
IonozorVojta <laitl.fko@gmail.com>
2016-06-09 19:42:05 +0200

    pathlocation.m converted onto -hdf5 friendly tool.

--- Octave/bin/pathlocation.m
+++ Octave/bin/pathlocation.m
@@ -16,32 +16,28 @@ A1 = "space.astro.cz/ionozor/VLF/HFKVARY/R2"
 A2 = "space.astro.cz/ionozor/VLF/HFKVARY/SOKOLOV-R1"
 B = "/"
 C = num2str(YY)
-D = "/"
 Ep = num2str(MM)
-F = "/"
-G = num2str(DD)
-H1 = "/t.txt"
-H2 = "/L.txt"
+F = num2str(DD)
+sid = "sid"
+format = ".h5"
+%working with a standard -hdf5 friendyl format
+G = "[sid YY MM DD format]"
 Ip = num2str(m)
 J = num2str(d)
-path01 = [A B C D Ip Ep F J G H1]
-path02 = [A B C D Ip Ep F J G H2]
-path11 = [A1 B C D Ip Ep F J G H1]
-path12 = [A1 B C D Ip Ep F J G H2]
-path21 = [A2 B C D Ip Ep F J G H1]
-path22 = [A2 B C D Ip Ep F J G H2]
+path01 = [A B C B Ip Ep B J F B G]
+path11 = [A1 B C B Ip Ep B J F B G]
+path21 = [A2 B C B Ip Ep B J F B G]
+%e.g. path01 = space.astro.cz/ionozor/VLF/2020/05/12/sid20200512.h5
 t = urlread(path01);
 L = urlread(path02);
 a = "ionozor/VLF/VLFOSTROV/R1/"
 b = num2str(YY)
 c = num2str(MM)
-local = [a b D Ip c]
+local = [a b B Ip c]
 cd(path0)
 cd (local)
 mkdir(num2str(DD))
 cd (num2str(DD))
-save -ascii t.txt t
-save -ascii L.txt L
 cd(path0)
 SID
 t = urlread(path11);
@@ -54,8 +50,6 @@ cd(path0)
 cd (local)
 mkdir(num2str(DD))
 cd (num2str(DD))
-save -ascii t.txt t
-save -ascii L.txt L
 cd(path0)
 SID
 t = urlread(path21);
