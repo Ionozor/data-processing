d58a65651c0eb868c2fb25973aee3a6bfa108537
IonozorVojta <laitl.fko@gmail.com>
2016-06-09 19:45:45 +0200

    -hdf5  friendly conversion done.
    pathlocation.m can now work within SID1.m and thus in -hdf5 friendly environment.

--- Octave/bin/pathlocation.m
+++ Octave/bin/pathlocation.m
@@ -28,8 +28,7 @@ path01 = [A B C B Ip Ep B J F B G]
 path11 = [A1 B C B Ip Ep B J F B G]
 path21 = [A2 B C B Ip Ep B J F B G]
 %e.g. path01 = space.astro.cz/ionozor/VLF/2020/05/12/sid20200512.h5
-t = urlread(path01);
-L = urlread(path02);
+data = urlread(path01);
 a = "ionozor/VLF/VLFOSTROV/R1/"
 b = num2str(YY)
 c = num2str(MM)
@@ -38,30 +37,25 @@ cd(path0)
 cd (local)
 mkdir(num2str(DD))
 cd (num2str(DD))
-cd(path0)
-SID
-t = urlread(path11);
-L = urlread(path12);
+SID1
+data = urlread(path11);
 a = "ionozor/VLF/HFKVARY/R2/"
 b = num2str(YY)
 c = num2str(MM)
-local = [a b D Ip c]
+local = [a b B Ip c]
 cd(path0)
 cd (local)
 mkdir(num2str(DD))
 cd (num2str(DD))
 cd(path0)
-SID
-t = urlread(path21);
-L = urlread(path22);
+SID1
+data = urlread(path21);
 a = "ionozor/VLF/HFKVARY/SOKOLOV-R1/"
 b = num2str(YY)
 c = num2str(MM)
-local = [a b D Ip c]
+local = [a b B Ip c]
 cd (local)
 mkdir(num2str(DD))
 cd (num2str(DD))
-save -ascii t.txt t
-save -ascii L.txt L
 cd(path0)
-SID
+SID1
