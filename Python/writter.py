2c5cc8c00dfa6eca166f542025fe77298dd547e2
IonozorVojta <laitl.fko@gmail.com>
2016-09-28 22:14:15 +0200

    FIX: .fits output

--- Python/writter.py
+++ Python/writter.py
@@ -6,40 +6,44 @@ import matplotlib.pylab as plt
 from astropy.io import fits
 
 sourceUrl = 'http://space.astro.cz/ionozor/VLF/OBSUPICE/OBSUPICE_VLF_R2/'
-
-year_start = np.asarray(time.strftime("%Y"),dtype='int')
+ 
+year_start = np.asarray(time.gmtime()[0],dtype='int')
 year_end = year_start
-month_start = np.asarray(str(time.strftime("%m")).zfill(2),dtype='int')
+month_start = np.asarray(str(time.gmtime()[1]).zfill(2),dtype='int')
 month_end = month_start
-day_start = np.asarray(str(time.strftime("%d")).zfill(2),dtype='int')
+day_start = np.asarray(str(time.gmtime()[2]).zfill(2),dtype='int')
+hour_start = np.asarray(str(time.gmtime()[3]).zfill(2),dtype='int')
+ 
+if hour_start<0:
+    day_start = day_start-1
+    hour_start = 23
+ 
 day_end = day_start
-hour_start = np.asarray(str(np.asarray(str(time.strftime("%H")),dtype='int')-2).zfill(2),dtype='int')
 hour_end = hour_start
 minute_start = np.asarray(str(1).zfill(2),dtype='int')
 minute_end = np.asarray(str(59).zfill(2),dtype='int')
-
+ 
 print('The initial time is given by:')
 print(year_start,month_start,day_start,hour_start,minute_start)
 print('The terminal time is given by:')
 print(year_end,month_end,day_end,hour_end,minute_end)
-
+ 
 def download_file(url):
-    local_filename = url.split('/')[-1]
+    local_filename = url.split('/')
     # note the stream=True parameter
     r = requests.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
+    with open('local_filename', 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
                 f.flush()
     return local_filename
-
-print('The souce URL is:')	
+ 
+print('The souce URL is:')  
 print(sourceUrl)
 download_file(sourceUrl)
-print('The local filename is:')
-print(local_filename)
-
+ 
+ 
 def download_fits(dir, year_start, month_start, day_start, hour_start, minute_start, year_end, month_end, day_end, hour_end, minute_end):
     '''
     Creates subdirectory 'snapshots' and downloads set of fits images from http://space.astro.cz/bolidozor/ from time period given in arguments. Returns nothing.
@@ -54,21 +58,21 @@ def download_fits(dir, year_start, month_start, day_start, hour_start, minute_st
     os.chdir(dir)
     con = bzpost.HTTPConnector(sourceUrl)
     con.connect()
-
+ 
     for snapshot in con.get_snapshots(datetime.datetime(year_start, month_start, day_start, hour_start, minute_start), datetime.datetime(year_end, month_end, day_end, hour_end, minute_end)):
        collector.append(snapshot.url) 
-
+ 
     for url in collector:
         download_file(url)
-
+ 
     con.close()
     os.chdir(wd)
-	
+     
 download_fits('snapshots', year_start, month_start, day_start, hour_start, minute_start, year_end, month_end, day_end, hour_end, minute_end)
 print('The directory and datetime are given by:')
 print('snapshots', year_start, month_start, day_start, hour_start, minute_start, year_end, month_end, day_end, hour_end, minute_end)
-
-
+ 
+ 
 def mkmosaic(dir, output='out.fits', axis='y', part=1, showplot=True):
     '''
     Concaternate specified parts of images from directory along the geometric axis. Plots the output by default. Having something else than fits files in target directory will likely result in error.
@@ -101,9 +105,9 @@ def mkmosaic(dir, output='out.fits', axis='y', part=1, showplot=True):
         plt.imshow(a)
         plt.show()
     del a #when axis were switched the variable caused skip of the else condition
-
+ 
 mkmosaic('snapshots', 'out.fits', 'y', 1, True)
-	
+     
 def split_single(image):
     """Split three channels raw file converted into fits format into respective files containing one channel. Color is appednde into filename.
     :image: Imput image.
@@ -118,9 +122,9 @@ def split_single(image):
     for i in ['r','g','b']:
         hdu = fits.PrimaryHDU(eval(i))
         hdu.writeto(filename + '_' + i + '.fits')
-    
+     
 split_single(out.fits)
-	
+     
 def split_dir(dir):
     """Iterate through fits images with multiple color channels and split their parts into fits with only one color channel and append that color to generated filename.
     :dir: The directory in which three channel fits are present.
@@ -136,9 +140,9 @@ def split_dir(dir):
         except:
             os.chdir(wd)
     os.chdir(wd)
-	
+     
 split_dir('snapshots')
-
+ 
 # apparently not needed, glob.glob
 def regex_dir(dir):
     """Return list of files ending with '.fits' from specified directory. """
@@ -151,5 +155,5 @@ def regex_dir(dir):
         except:
             pass
     return collector
-	
+     
 regex_dir('snapshots')
